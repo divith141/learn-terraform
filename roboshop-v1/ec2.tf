f1db97dc6ed721ac0d6ba07444e8f074ca0b4272
Kranthi <kranthi.nanu@gmail.com>
2024-08-08 01:03:50 -0700

    Terraform code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -1,29 +1,29 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0361b4cfb8e9eb9ab"]
   tags = {
-    Name = "frontend"
+    Name = "frontend.dev"
   }
 }
 
 
 resource "aws_instance" "mongo" {
   ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0361b4cfb8e9eb9ab"]
   tags = {
-    Name = "mongo"
+    Name = "mongo.dev"
   }
 }
 
 
 resource "aws_instance" "catalogue" {
   ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0361b4cfb8e9eb9ab"]
   tags = {
-    Name = "catalogue"
+    Name = "catalogue.dev"
   }
 }
 
