02c89563c5ce2ba499c348670679e1e80ca94072
Kranthi <kranthi.nanu@gmail.com>
2024-08-09 01:52:32 -0700

    Terraform code

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -2,8 +2,8 @@
 # = expression ? TRUE_VAL : FALSE_VAL
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z00376861T6KFA01SJSIS"
-  name    = "test.rdevopsb80.online"
+  zone_id = "Z043863937TBAZZ058716"
+  name    = "test.kranthi.online"
   type    = "A"
   ttl     = var.ttl == "" ? 15 : var.ttl
   records = ["1.1.1.1"]
